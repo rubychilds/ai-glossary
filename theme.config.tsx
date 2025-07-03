87dc3d1e7438f7f0edf8522e55d6a81b42bf73b4
ruby childs <ruby.childs1@gmail.com>
2025-07-02 22:30:31 -0400

    adding logos

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,13 +2,18 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span ><img src="/Ruby.png" alt="Ruby logo" width="100px" />: AI Glossary</span>,
+  logo: (
+    <span style={{ display: 'flex', alignItems: 'center', gap: '0.5em' }}>
+      <img src="/Ruby.png" alt="Ruby logo" width="100px" style={{ display: 'inline-block', verticalAlign: 'middle' }} />
+      <span>AI Glossary</span>
+    </span>
+  ),
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/rubychilds/ai-glossary',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/rubychilds/ai-glossary',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'AI Glossary',
   },
 }
 
